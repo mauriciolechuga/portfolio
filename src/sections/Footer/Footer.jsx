b063dd44c2f8be5d9a7bbe012945df0b7d7b86a1
Mauricio Lechuga <mlechugasanchez2857@conestogac.on.ca>
2025-06-05 11:36:12 -0400

    feat: add Footer component and styles, update App structure

---
+++ src/sections/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from "./FooterStyles.module.css";
+
+function Footer() {
+  return (
+    <section id="footer" className={styles.container}>
+      <p>&copy; 2025 Mauricio Lechuga</p>
+    </section>
+  );
+}
+
+export default Footer;
